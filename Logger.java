d8c60a1ebff30920f2d76851d701a48f95fe8702
pedro09pm <pedro09pm.principal@gmail.com>
2022-10-31 23:47:36 +0100

    Formatting and Enum properties

--- Logger.java
+++ Logger.java
@@ -17,7 +17,7 @@ public class Logger {
 
     final private static boolean logWarning = true;
     final private static boolean logInfo = true;
-    final private static boolean logDebug = false;
+    final private static boolean logDebug = true;
 
     final private static String logPath = "logs/";
     final private static String defaultLogFile = "DefaultLog.txt";
@@ -29,19 +29,26 @@ public class Logger {
 
         // LogType(isShown, includesTime, padding)
 
-        WARNING(true, true, 1),
-        INFO(true, true, 4),
-        DEBUG(true, true, 3),
-        REQUIRED(false, false, 0); // Required log's tags and time are not shown.
+        // For formatting reasons if isTagShown == false -> padding = 0
 
+        HEADER(true, false, false, 0),
+        WARNING(logWarning, true, true, 1),
+        INFO(logInfo, true, true, 4),
+        DEBUG(logDebug, true, true, 3),
+        REQUIRED(true, true, true, 0); // [Biggest type, length is 8]
+
+        private final boolean isLogged;
         private final boolean isTagShown;
         private final boolean includesTime;
         private final int padding;
 
-        private LogType (boolean isTagShown, boolean includesTime, int padding) {
+        private LogType (boolean isLogged, boolean isTagShown, boolean includesTime, int padding) {
+
+            this.isLogged = isLogged;
             this.isTagShown = isTagShown;
             this.includesTime = includesTime;
             this.padding = padding;
+
         }
 
     }
@@ -84,16 +91,52 @@ public class Logger {
 
     public static void log(String log, LogType type) {
 
+        // Format log.
 
+        if (type == LogType.HEADER) {
+
+            // Write log without modifications.
+
+        }
+
+        if (type.isLogged && type != LogType.HEADER) {
+
+            if (type.isTagShown) {
+
+                log = " ".repeat(type.padding + 1) + "[" + type + "] : " + log;
+
+            } else {
+
+                log = " ".repeat(10 + type.padding + 1) + " : " + log;
+
+            }
+
+            if (type.includesTime) {
+
+                log = "[" + getCurrentTime() + "]" + log;
+
+            } else {
+
+                log = " ".repeat(21) + log;
+
+            }
+
+        }
+
+        // Write log to file.
+
+        System.out.println(log);
 
     }
 
 
     public static void main(String[] args) {
 
-        System.out.println(getCurrentTime() + " ".repeat(LogType.REQUIRED.padding + 1) + "[" + LogType.REQUIRED + "] : ");
-        System.out.println(getCurrentTime() + " ".repeat(LogType.WARNING.padding + 1) + "[" + LogType.WARNING + "] : ");
-        System.out.println(getCurrentTime() + " ".repeat(LogType.DEBUG.padding + 1) + "[" + LogType.DEBUG + "] : ");
+        log("El wiwi", LogType.HEADER);
+        log("El wiwi", LogType.WARNING);
+        log("El wiwi", LogType.INFO);
+        log("El wiwi", LogType.DEBUG);
+        log("El wiwi", LogType.REQUIRED);
 
     }
 
