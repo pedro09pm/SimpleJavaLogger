cf6ecda283e1ea328e395520982c4b37f87ebc96
pedro09pm <pedro09pm.principal@gmail.com>
2023-06-21 01:25:52 +0200

    Update to Maven project

---
+++ README.md
@@ -0,0 +1,143 @@
+
+
+<div style="text-align: justify">
+
+<div style="text-align: center">
+
+# SIMPLE JAVA LOGGER
+
+</div>
+
+Lisenced under **MIT**, created by **Pedro Marín Sanchis**.
+
+**READ LICENSE BEFORE USE.**
+
+---
+
+### DOWNLOAD
+
+Head to the [releases](https://github.com/pedro09pm/SimpleJavaLogger/releases) page of this repository, where you'll find all the different versions.
+
+### HOW TO USE
+
+First of all, before logging anything, **you must create a Logger object**. This can be done by using the **constructor**:
+
+```java
+Logger logger = new Logger("Log Path", "Header Text");
+```
+
+The default path for logs is "**logs**". They will look like so:
+
+```
+Log[2022-11-12_00-41-06][1].log
+Log[2022-11-12_04-37-16][1].log
+```
+
+To log something use the **log()** method:
+
+```java
+logger.log("Your log text here", Logger.LogType.INFO);
+```
+
+**REMEMBER TO CLOSE THE LOGGER BEFORE PROGRAM EXIT.** Otherwise you could encounter your logs missing entries, or not existing at all. By default the logger will write to file every **100** entries.
+
+Use the **close()** method.
+```java
+logger.close();
+```
+
+You can also use the **writeToFile()** method to force the logger to write to the file.
+
+```java
+logger.writeToFile();
+```
+
+Finally, if you want to change the automatic logging interval you can use the **setWriteToFileInterval()** method.
+```java
+logger.setWriteToFileInterval(50);
+```
+
+### LOGS
+
+#### · LOG TYPES
+
+Logs can be of various types, which come with specific properties that change their apprearance in the final log file. All log types except LogType.REQUIRED have a boolean flag declared at class level which can be changed to decide if they are logged or not.
+
+|                  |      **HEADER**     |     **WARNING**     |       **INFO**      |      **DEBUG**      | **REQUIRED** |
+|:----------------:|:-------------------:|:-------------------:|:-------------------:|:-------------------:|:------------:|
+| **isTagShown**   | false               | true                | true                | true                | true         |
+| **includesTime** | false               | true                | true                | true                | true         |
+
+Default log types have the following apperance, note that LogType.HEADER has neither a date nor a type label.
+
+
+```
+Test
+[2022-11-12_00-41-06]  [WARNING] : Test
+[2022-11-12_00-41-06]     [INFO] : Test
+[2022-11-12_00-41-06]    [DEBUG] : Test
+[2022-11-12_00-41-06] [REQUIRED] : Test
+```
+
+#### · DELETING OLD LOG FILES
+
+To delete old log files you can use the **deleteOldLogs()** method.
+
+```java
+Logger.deleteOldLogs("Log Path");
+```
+
+This will delete all files ending with ".log" inside the specified directory.
+
+#### · DEACTIVATING LOGGING FOR SPECIFIC TYPES:
+
+Say you want to make it so LogType.DEBUG logs are not logged. Use the **changeLogTypeLogging()** method.
+
+```java
+Logger.changeLogTypeLogging(Logger.LogType.DEBUG, false);
+```
+
+---
+
+### HOW TO MODIFY
+
+#### · ADDING LOG TYPES
+
+Inside Logger.java you will find the enum "LogType":
+
+```java
+public static enum LogType {
+    // LogType(isShown, includesTime).
+    HEADER(false, false),
+    WARNING(true, true),
+    INFO(true, true),
+    DEBUG(true, true),
+    REQUIRED(true, true);
+
+    private final boolean isTagShown;
+    private final boolean includesTime;
+
+    private LogType (boolean isTagShown, boolean includesTime) {
+        this.isTagShown = isTagShown;
+        this.includesTime = includesTime;
+    }
+}
+```
+
+Adding new log types is simple, it can be done by adding an element to the list like so:
+
+```java
+public static enum LogType {
+    // LogType(isShown, includesTime).
+    HEADER(false, false),
+    WARNING(true, true),
+    INFO(true, true),
+    DEBUG(true, true),
+    REQUIRED(true, true),
+    NEWTYPE(true, true); // <----- Your new log type!
+    ...
+```
+
+Keep in mind that the last element of the list must end with ";" and the rest of the elements must be separated by commas. Spacing for formatting and such is calculated automatically.
+
+</div>
---
+++ src/main/java/Logger.java
@@ -0,0 +1,186 @@
+// Simple Java Logger by Pedro Marín Sanchis, licensed under MIT.
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.time.LocalTime;
+import java.time.LocalDate;
+
+
+public class Logger {
+    // Configuration.
+    private int storedEntries = 0;
+    private int writeToFileInterval = 100;
+    private String logFilePath = "logs" + File.separatorChar;
+    private String currentLog = "DefaultLog";
+    private String logHeaderText = "SIMPLE JAVA LOGGER\n\n" + "-".repeat(50); // Default Header Text.
+
+    // Log.
+    private StringBuffer logBuffer = new StringBuffer();
+    private File logFile = null;
+
+    // LogType.
+    private static byte[] paddingArray;
+    private static byte biggestLogTypeLength = 0;
+    private boolean[] isLoggedArray = {true, true, true, true, true,}; // Must be declared in the same order as enum LogType [HEADER, WARNING...].
+
+    public static enum LogType {
+        // LogType(isShown, includesTime).
+        HEADER(false, false),
+        WARNING(true, true),
+        INFO(true, true),
+        DEBUG(true, true),
+        REQUIRED(true, true);
+
+        private final boolean isTagShown;
+        private final boolean includesTime;
+
+        private LogType (boolean isTagShown, boolean includesTime) {
+            this.isTagShown = isTagShown;
+            this.includesTime = includesTime;
+        }
+    }
+
+    public Logger(String logFilePath, String logHeaderText) {
+        if (logHeaderText != null) {
+            this.logFilePath = logFilePath + File.separatorChar;
+        }
+
+        if (logHeaderText != null) {
+            this.logHeaderText = logHeaderText;
+        }
+
+        generateIsLoggedArray();
+        generatePaddingArray();
+        currentLog = "Log["+getCurrentTime()+"]";
+        logFile = findAvailableLog();
+        log(this.logHeaderText, LogType.HEADER);
+        writeToFile();
+    }
+
+    public void setWriteToFileInterval(int writeToFileInterval) {
+        this.writeToFileInterval = writeToFileInterval;
+    }
+
+    private File findAvailableLog() {
+        int count = 1;
+        while (true) {
+            File file = new File(logFilePath+currentLog+"["+count+"].log");
+            if (!file.exists()) {
+                return file;
+            }
+            count++;
+        }
+    }
+
+    private static void generatePaddingArray() {
+
+        paddingArray = new byte[LogType.values().length];
+
+        LogType biggestLogType = LogType.values()[0];
+
+        for (int i = 0; i < LogType.values().length; i++) { // Find biggest logType by length
+
+            if (LogType.values()[i].toString().length() > biggestLogType.toString().length()) {
+
+                biggestLogType = LogType.values()[i];
+
+            }
+
+        }
+
+        biggestLogTypeLength = (byte) biggestLogType.toString().length();
+
+        for (int i = 0; i < LogType.values().length; i++) { // Create paddingArray
+
+            paddingArray[i] = (byte) (biggestLogTypeLength - LogType.values()[i].toString().length());
+
+        }
+
+    }
+
+    private void generateIsLoggedArray() { // Establish isLoggedArray with the length of LogType enum and set all values to TRUE as default.
+        isLoggedArray = new boolean[LogType.values().length];
+        for (int i = 0; i < isLoggedArray.length; i++) {
+            isLoggedArray[i] = true;
+        }
+    }
+
+    public void changeLogTypeLogging(LogType type, boolean isLogged) {
+        isLoggedArray[type.ordinal()] = isLogged;
+    }
+
+    public void changeLogPath(String logFilePath) {
+        this.logFilePath = logFilePath;
+    }
+
+    public static void deleteOldLogs(String path) {
+        File[] oldLogs = new File(path+File.separatorChar).listFiles();
+        for (int i = 0; i < oldLogs.length; i++) {
+            if (oldLogs[i].getName().endsWith(".log")) {
+                oldLogs[i].delete();
+            }
+        }
+    }
+
+    private static String getCurrentTime() {
+        return LocalDate.now().toString() + "_" + LocalTime.now().toString().substring(0, 8).replaceAll(":", "-");
+    }
+
+    private static String getFormattedLog(String log, LogType type) {
+        int spacing = 1; // Spacing between date and LogType.
+
+        if (!type.isTagShown && !type.includesTime) {
+            return log + "\n"; // Return log without modifications (Except newline, of course).
+        } else {
+            if (type.isTagShown) {
+                log = " ".repeat(paddingArray[type.ordinal()] + spacing) + "[" + type + "] : " + log;
+            } else {
+                log = " ".repeat(biggestLogTypeLength + spacing*2) + " : " + log;
+            }
+
+            if (type.includesTime) {
+                log = "[" + getCurrentTime() + "]" + log;
+            } else {
+                log = " ".repeat(getCurrentTime().length() + spacing*2) + log;
+            }
+        }
+
+        return log + "\n";
+    }
+
+    public void log(String log, LogType type) {
+        // Format log.
+        if (!isLoggedArray[type.ordinal()]) {return;} // Do not log if isLoggedArray says so.
+
+        logBuffer.append(getFormattedLog(log, type));
+        storedEntries++;
+        if (storedEntries>=writeToFileInterval) {
+            writeToFile();
+        }
+
+    }
+
+    public void writeToFile() {
+        try {
+            FileWriter writer = new FileWriter(logFile);
+            writer.write(logBuffer.toString());
+            writer.close();
+            storedEntries = 0;
+        } catch (IOException e) {
+            log("Error while trying to create log " + currentLog + ".", LogType.REQUIRED);
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        log("\nEND OF LOG.", LogType.HEADER);
+        writeToFile();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        close();
+    }
+}
